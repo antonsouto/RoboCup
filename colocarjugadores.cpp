399297459bc70900350e275b8853b751786f7fc8
MartinMatoArjones <martin.mato1211@gmail.com>
2024-06-28 17:40:06 +0200

    zonas arregladas pero fallo al salir de ellas

--- colocarjugadores.cpp
+++ colocarjugadores.cpp
@@ -1,47 +1,61 @@
-#include<vector>
-#include<iostream>
-#include"colocarjugadores.h"
-#include"stringutilities.h"
-
+#include <vector>
+#include <iostream>
+#include "colocarjugadores.h"
+#include "stringutilities.h"
 
 using namespace std;
-
 
 // Inicializa cada jugador en el campo en función del mensaje que retorne el servidor en la primera consulta, ej: (init l 1 before_kick_off)
 // devuelve un string con el mensaje de posicion para este jugador, por ejemplo (move -35 28)
 
-string inicializoJugador(const string &respuesta){
-    vector<string>vrespuesta = separarPalabras(respuesta); 
-    string mensaje; //mensaje que se le envia al servidor para que instancie los jugadores en sus posiciones iniciales
+string inicializoJugador(const string &respuesta)
+{
+    vector<string> vrespuesta = separarPalabras(respuesta);
+    string mensaje; // mensaje que se le envia al servidor para que instancie los jugadores en sus posiciones iniciales
 
-    if(vrespuesta[2]=="1"){ //(init l 1 before_kick_off)
-        mensaje ="(move -51 0)";
-    }else if(vrespuesta[2]=="2"){
-        mensaje ="(move -35 -28)";
-    }else if(vrespuesta[2]=="3"){
-        mensaje ="(move -42 -14)";
-    }else if(vrespuesta[2]=="4"){
-        mensaje ="(move -42 14)";
-    }else if(vrespuesta[2]=="5"){
-        mensaje ="(move -35 28)";
-    }else if(vrespuesta[2]=="6"){
-        mensaje ="(move -25 11)";
-    }else if(vrespuesta[2]=="7"){
-        mensaje ="(move -8 20)";
-    }else if(vrespuesta[2]=="8"){
-        mensaje ="(move -25 -11)";
-    }else if(vrespuesta[2]=="9"){
-        mensaje ="(move -5 0)";
-    }else if(vrespuesta[2]=="10"){
-        mensaje ="(move -15 0)";
-    }else if(vrespuesta[2]=="11"){
-        mensaje ="(move -8 -20)";
+    if (vrespuesta[2] == "1")
+    { //(init l 1 before_kick_off)
+        mensaje = "(move -51 0)";
     }
-
+    else if (vrespuesta[2] == "2")
+    {
+        mensaje = "(move -35 -28)";
+    }
+    else if (vrespuesta[2] == "3")
+    {
+        mensaje = "(move -42 -14)";
+    }
+    else if (vrespuesta[2] == "4")
+    {
+        mensaje = "(move -42 14)";
+    }
+    else if (vrespuesta[2] == "5")
+    {
+        mensaje = "(move -35 28)";
+    }
+    else if (vrespuesta[2] == "6")
+    {
+        mensaje = "(move -25 11)";
+    }
+    else if (vrespuesta[2] == "7")
+    {
+        mensaje = "(move -8 20)";
+    }
+    else if (vrespuesta[2] == "8")
+    {
+        mensaje = "(move -25 -11)";
+    }
+    else if (vrespuesta[2] == "9")
+    {
+        mensaje = "(move -5 0)";
+    }
+    else if (vrespuesta[2] == "10")
+    {
+        mensaje = "(move -15 0)";
+    }
+    else if (vrespuesta[2] == "11")
+    {
+        mensaje = "(move -8 -20)";
+    }
     return mensaje;
-
-} 
-
-
-
-
+}
--- funciones_de_juego.cpp
+++ funciones_de_juego.cpp
@@ -12,7 +12,7 @@
 
 using namespace std;
 bool enJuego = false;
-struct visioncampo micontainer;
+visioncampo micontainer;
 
 bool Escuchar(string received_message_content, string ladoJugador)
 {
@@ -30,7 +30,6 @@ string Ver(string received_message_content, string ladoJugador, string numerojug
     string resultado;
     pair<float, float> coordenadas = rellenaContenedor(micontainer, received_message_content);
     cout << "\n\t Mis coordenadas en este momento son : " << coordenadas.first << " " << coordenadas.second << endl;
-
     if (received_message_content.find("(b) ") != -1)
     {
         auto balon = buscarValores(received_message_content, "((b) ");
@@ -82,6 +81,7 @@ string Ver(string received_message_content, string ladoJugador, string numerojug
         }
         else if (zonaJuego(numerojugadorvisto, ladoJugador, coordenadas)) // Cuando ve la pelota y esta lejos
         {
+
             if (stoi(balon.first) > 80)
             {
                 return resultado = "(turn " + balon.second + ")";
@@ -105,7 +105,7 @@ string Ver(string received_message_content, string ladoJugador, string numerojug
         }
         else
         {
-            return resultado = "(dash 0 " + balon.second + ")";
+            return resultado = "(dash -5 " + balon.second + ")";
         }
         // Aquí puedes realizar otras operaciones con los valores extraídos
     }
@@ -140,43 +140,43 @@ bool zonaJuego(string numerojugador, string lado, pair<float, float> coordenadas
 
     if (numerojugador == "1")
     {
-        return ((37 <= x <= 51) && (y < 19));
+        return ((37 <= x && x <= 52) && (y < 19));
     }
     else if (numerojugador == "2" || numerojugador == "5")
     {
-        return ((25 <= y <= 33));
+        return ((25 <= y && y <= 33));
     }
     else if (numerojugador == "3" || numerojugador == "4")
     {
-        return ((25 <= x <= 51) && (0 <= y <= 25));
+        return ((25 <= x && x <= 52) && (0 <= y && y <= 25));
     }
     else if ((numerojugador == "6" || numerojugador == "8"))
     {
-        return ((10 <= x <= 40) && (0 <= y <= 25));
+        return ((10 <= x && x <= 40) && (0 <= y && y <= 25));
     }
     else if ((numerojugador == "7" || numerojugador == "11") && (lado == "l"))
     {
-        return ((-10 <= coordenadas.first <= 40) && (5 <= y <= 33));
+        return ((-10 <= coordenadas.first && coordenadas.first <= 40) && (5 <= y && y <= 33));
     }
     else if ((numerojugador == "7" || numerojugador == "11") && (lado == "r"))
     {
-        return ((10 >= coordenadas.first >= -40) && (5 <= y < 33));
+        return ((10 >= coordenadas.first && coordenadas.first >= -40) && (5 <= y && y <= 33));
     }
     else if ((numerojugador == "10") && (lado == "l"))
     {
-        return ((-25 <= coordenadas.first <= 40) && (0 <= y <= 25));
+        return ((-25 <= coordenadas.first && coordenadas.first <= 40) && (0 <= y && y <= 25));
     }
     else if ((numerojugador == "10") && (lado == "r"))
     {
-        return ((25 >= coordenadas.first >= -40) && (0 <= y < 25));
+        return ((25 >= coordenadas.first && coordenadas.first >= -40) && (0 <= y && y <= 25));
     }
     else if ((numerojugador == "9") && (lado == "l"))
     {
-        return ((-15 >= coordenadas.first <= 50) && (0 <= y <= 25));
+        return ((-15 <= coordenadas.first && coordenadas.first <= 50) && (0 <= y && y <= 25));
     }
     else if ((numerojugador == "9") && (lado == "r"))
     {
-        return ((15 >= coordenadas.first >= -50) && (0 <= y < 25));
+        return ((-50 <= coordenadas.first && coordenadas.first <= 15) && (0 <= y && y <= 25));
     }
     else
     {
